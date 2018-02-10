0e30c80f0fbafdc16a3c0e93d9f3c685c93d3562
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-02-10 06:12:31 +0100

    publish compiles a new report now, too. And the report is copied twice: as current file and with a date stamp, so that i can review my history...

--- publish.sh
+++ publish.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 #
+# Generate a new report
+#
+cd Source
+Rscript -e "rmarkdown::render('./Dashboard.Rmd')"
+cd ..
+
+#
 # Publish Learning Progress to Reporting Repository
 #
 cp -f Source/Dashboard.html ../Overview/docs/Learning-Dashboard.html
+now=$(date +"%Y-%m-%d")
+cp -f Source/Dashboard.html ../Overview/docs/Learning-Dashboard-$now.html
 cd ../Overview
-git add docs/Learning-Dashboard.html
+git add .
 git commit -m "Update for Learning Dashboard"
 git push
 cd ../LearningProgress-R
